d7e1e8daece4577d87fed186c4e6ca3e78633598
Eddie Aponte <ed.aponte0@gmail.com>
2023-03-16 00:47:45 -0400

    aaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,17 @@
 use core::panic;
-use std::fmt::format;
-
 use gcloud_sdk::{
-    google::cloud::secretmanager::v1::{
-        secret_manager_service_client::SecretManagerServiceClient, CreateSecretRequest,
-        ListSecretsRequest, Secret, SecretVersion,
+    google::cloud::{
+        secretmanager::v1::{
+            replication::Automatic, secret_manager_service_client::SecretManagerServiceClient,
+            AddSecretVersionRequest, CreateSecretRequest, ListSecretsRequest, Replication, Secret,
+            SecretVersion,
+        },
+        secrets,
     },
+    proto_ext::secretmanager::SecretPayload,
     GoogleApi, GoogleAuthMiddleware, GoogleEnvironment,
 };
+use std::collections::{hash_map::RandomState, HashMap};
 
 mod gcloudsecret;
 #[tokio::main]
@@ -18,10 +22,10 @@ async fn main() {
         Err(error) => panic!("Failed to create gcloud client: {}", error),
     };
     let client2 = client.clone();
-    match vault(client).await {
-        Ok(result) => result,
-        Err(error) => panic!("Failed to list gcloud secrets: {}", error),
-    }
+    // match vault(client).await {
+    //   Ok(result) => result,
+    //Err(error) => panic!("Failed to list gcloud secrets: {}", error),
+    //}
 
     match insert(client2).await {
         Ok(result) => result,
@@ -29,7 +33,7 @@ async fn main() {
     }
 }
 // lists secrets
-async fn vault(
+async fn _vault(
     client: GoogleApi<SecretManagerServiceClient<GoogleAuthMiddleware>>,
 ) -> Result<(), Box<dyn std::error::Error>> {
     let google_project_id = GoogleEnvironment::detect_google_project_id().await
@@ -52,15 +56,47 @@ async fn insert(
 ) -> Result<(), Box<dyn std::error::Error>> {
     let google_project_id: String = GoogleEnvironment::detect_google_project_id().await.expect("No Google Project ID detected. Please specify it explicitly using env variable: PROJECT_ID");
 
+    // this fucking sucks
+    let secrets = Some(Secret {
+        name: "test".to_string(),
+        replication: Some(Replication {
+            replication: Option::Some(
+                gcloud_sdk::google::cloud::secretmanager::v1::replication::Replication::Automatic(
+                    Automatic {
+                        customer_managed_encryption: None,
+                    },
+                ),
+            ),
+        }),
+        create_time: None,
+        etag: "".to_string(),
+        rotation: None,
+        expiration: None,
+        labels: HashMap::from([("".to_string(), "".to_string())]),
+        topics: Vec::from([]),
+        version_aliases: HashMap::from([("".to_string(), 0)]),
+        annotations: HashMap::from([("".to_string(), "".to_string())]),
+    });
+
     let response = client
         .get()
         .create_secret(tonic::Request::new(CreateSecretRequest {
             parent: format!("projects/{}", google_project_id),
-            secret_id: format!("fdsfadf"),
-            secret: std::option::Option::None,
+            secret_id: format!("TestSecret-RS"),
+            secret: secrets,
         }))
         .await?;
     println!("Response: {:?}", response);
+
+    let version =
+        client
+            .clone()
+            .get()
+            .add_secret_version(tonic::Request::new(AddSecretVersionRequest {
+                parent: format!("TestSecret-RS"),
+                payload: todo!(),
+            }));
+    //println!("Response: {:?}", version);
 
     Ok(())
 }
